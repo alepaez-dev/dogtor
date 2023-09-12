d7176fd81fbd67ffa85bf432badb96421676e5f9
Alejandra Paez <alepaezdev@gmail.com>
2023-09-11 19:57:33 -0600

    feat: ListView

--- dogtor/vet/urls.py
+++ dogtor/vet/urls.py
@@ -4,6 +4,8 @@
 from django.urls import path
 
 # Views
+# PascalCase -> clases -> modelos o vistas clase
+# snake_case -> para todo lo demas
 from .views import list_pet_owners, Test, OwnersList
 
 urlpatterns = [
--- dogtor/vet/views.py
+++ dogtor/vet/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView
 
 
 # Models
@@ -28,20 +28,31 @@ def list_pet_owners(request):
     return HttpResponse(template.render(context, request))
 
 
-# PascalCase
+# CRUD
+
+# Ctrl + /
+# Cmd + /
+# class OwnersList(TemplateView):
+#     # Renderizame este template
+#     template_name = "vet/owners/list.html"
+
+#     # Que este template va a tener cierto 'contexto'
+#     def get_context_data(self, **kwargs):
+#         # Agarrar el contexto que ya creo 'TemplateView'
+#         context = super().get_context_data(**kwargs)
+
+#         # Le agregamos nuestro custom context
+#         context["owners"] = PetOwner.objects.all()
+#         return context
 
 
-class OwnersList(TemplateView):
-    # Renderizame este template
-    template_name = "vet/owners/list.html"
-
-    # Que este template va a tener cierto 'contexto'
-    def get_context_data(self, **kwargs):
-        # Agarrar el contexto que ya creo 'TemplateView'
-        context = super().get_context_data(**kwargs)
-        # Le agregamos nuestro custom context
-        context["owners"] = PetOwner.objects.all()
-        return context
+class OwnersList(ListView):
+    # 1. Modelo que estamos manipulando
+    # 2. Template con el que vamos renderizar
+    # 3. El contexto que va a tener ese template
+    model = PetOwner  # 1
+    template_name = "vet/owners/list.html"  # 2
+    context_object_name = "owners"  # 3
 
 
 class Test(View):
