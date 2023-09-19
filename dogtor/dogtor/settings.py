c040db23299b92e185f7b805a7c581f351720f53
Alejandra Paez <alepaezdev@gmail.com>
2023-09-18 19:40:28 -0600

    feat: login form

--- dogtor/dogtor/settings.py
+++ dogtor/dogtor/settings.py
@@ -118,6 +118,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# URL's de redireccion para login
+LOGIN_REDIRECT_URL = "vet/owners/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
