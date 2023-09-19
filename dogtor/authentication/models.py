761c87594439d79b1dbeff7bc050d2f96a2374d1
Alejandra Paez <alepaezdev@gmail.com>
2023-09-18 20:26:26 -0600

    feat: add ModUser

---
+++ dogtor/authentication/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+# Create your models here.
+
+
+class ModUser(AbstractBaseUser, PermissionsMixin):
+    """Custom Moderator User."""
+
+    # Sobreescribir propiedad del modelo User
+    # Extender (nuevos fields)
+
+    email = models.EmailField(unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=200, blank=True)
+    start_date = models.DateTimeField(auto_now_add=True)
+    about = models.TextField(max_length=500)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    # Campo identificador del user
+    USERNAME_FIELD = "email"
+
+    # Campos requeridos para cuando usamos el comando createsuperuser
+    REQUIRED_FIELDS = ["user_name"]
+
+    # Metodo string
+    def __str__(self):
+        return f"{self.user_name} {self.email} "
--- dogtor/dogtor/settings.py
+++ dogtor/dogtor/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # Custom apps
     "vet",
     "blog",
+    "authentication",
 ]
 
 MIDDLEWARE = [
